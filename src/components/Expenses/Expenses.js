bd4ef898c9e7947340b9c70ba0471ff362449714
Chetna Aggarwal <caggarw@monsanto.com>
2021-08-18 09:29:57 -0500

    added expenseslist

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import "./Expenses.css";
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 const Expenses = (props) => {
   // const expenses = props.expenses;
   const [enteredYear, setEnteredYear] = useState("2021");
@@ -15,6 +15,7 @@ const Expenses = (props) => {
   const filteredExpenses = props.expenses.filter((expense) => {
     return expense.date.getFullYear().toString() === enteredYear;
   });
+
   return (
     <div>
       <Card className="expenses">
@@ -22,17 +23,7 @@ const Expenses = (props) => {
           selected={enteredYear}
           onChangeYear={yearChangeHandler}
         />
-        {filteredExpenses.map((expense) => {
-          console.log("year is ", enteredYear);
-          return (
-            <ExpenseItem
-              key={expense.id}
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date}
-            />
-          );
-        })}
+        <ExpensesList expenses={filteredExpenses} />
       </Card>
     </div>
   );
